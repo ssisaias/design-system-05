9525c01c6d49e2a27f8bff292eb57e108154c5d6
ssisaias <ssisaias@users.noreply.github.com>
2024-04-21 23:13:38 -0300

    Adds Button Story

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Button } from "@isaias-ui/react";
+import { Children } from "react";
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    title: 'Enviar'
+  }
+} as Meta
+
+export const Primary: StoryObj = {
+}
+
+export const Secondary: StoryObj = {
+
+}
